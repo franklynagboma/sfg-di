d164abde89d8cdf9c78fb298863591b0dd1c518b
frankynagboma <franklyn1great@gmail.com>
2022-04-04 10:13:41 +0100

    Assignment with DI.

--- src/main/java/com/example/sfgdi/services/CatPetService.java
+++ src/main/java/com/example/sfgdi/services/CatPetService.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 @Profile("cat")
-@Service("petService")
+@Service("pet")
 public class CatPetService implements PetService {
     @Override
     public String getPetType() {
--- src/main/java/com/example/sfgdi/services/DogPetService.java
+++ src/main/java/com/example/sfgdi/services/DogPetService.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 @Profile({"dog", "default"})
-@Service("petService")
+@Service("pet")
 public class DogPetService implements PetService {
 
     @Override
